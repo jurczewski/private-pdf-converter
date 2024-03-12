ef299446c6e2b2adf09efc583128d4c9b9224ef7
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2024-03-12 23:15:48 +0000

    add pdf command

---
+++ src/Commands/DirToPdf.cs
@@ -0,0 +1,9 @@
+namespace PrivatePdfConverter.Commands;
+
+public static class DirToPdf
+{
+    public static void ConvertDirectoryToOnePdf(string path, string? output)
+    {
+
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Cocona;
 using PrivatePdfConverter;
+using PrivatePdfConverter.Commands;
 
 var app = CoconaLiteApp.Create();
+
+app.AddCommand("pdf", DirToPdf.ConvertDirectoryToOnePdf);
 
 Logger.Initialize();
 
