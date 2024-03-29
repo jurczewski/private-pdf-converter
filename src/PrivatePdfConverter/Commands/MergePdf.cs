129e8ff615946e556d4e578b602c798d95c4337d
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2024-03-29 15:38:25 +0100

    merge - feature

--- src/PrivatePdfConverter/Commands/MergePdf.cs
+++ src/PrivatePdfConverter/Commands/MergePdf.cs
@@ -1,9 +1,33 @@
+using iText.Kernel.Pdf;
+using Serilog;
+
 namespace PrivatePdfConverter.Commands;
 
 public static class MergePdf
 {
     public static void ConvertDirectoryToOnePdf(string path, string? output)
     {
+        var filesPaths = Directory.GetFiles(path, "?.pdf").ToList();
 
+        Log.Logger.Information("Read {Count} files from '{Path}'", filesPaths.Count, path);
+        filesPaths.ForEach(x => Log.Logger.Information("File name: {FileName}", Path.GetFileName(x)));
+        var pdfs = filesPaths.Select(x => new PdfDocument(new PdfReader(x))).ToList();
+        var outputFileName = string.IsNullOrEmpty(output) ? "output.pdf" : output;
+
+        // check if filename already has .pdf extension and add .pdf at the end if
+        if (!outputFileName.EndsWith(".pdf"))
+        {
+            outputFileName += ".pdf";
+        }
+
+        using var mergedDocument = new PdfDocument(new PdfWriter($"{path}/{outputFileName}"));
+        foreach (var pdf in pdfs)
+        {
+            pdf.CopyPagesTo(1, pdf.GetNumberOfPages(), mergedDocument);
+        }
+
+        pdfs.ForEach(x => x.Close());
+
+        Log.Logger.Information("PDF '{OutputFileName}' created at '{Path}'", outputFileName, $"{path}/{outputFileName}");
     }
 }
