0154a7c4cfd31fa847b65171601cc8414b215935
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2024-04-03 12:12:32 +0200

    LoadFilePathsFromDirectory

--- src/PrivatePdfConverter/Services/FileService.cs
+++ src/PrivatePdfConverter/Services/FileService.cs
@@ -1,6 +1,16 @@
+using Serilog;
+
 namespace PrivatePdfConverter.Services;
 
 public static class FileService
 {
+    public static IEnumerable<string> LoadFilePathsFromDirectory(this string path, string searchPattern = "*")
+    {
+        var files = Directory.GetFiles(path, searchPattern).ToList();
 
+        Log.Logger.Information("Read {Count} files from '{Path}'", files.Count, path);
+        files.ForEach(x => Log.Logger.Information("File name: {FileName}", Path.GetFileName(x)));
+
+        return files;
+    }
 }
