7d000e84e09fff34d1d5d27277f0e3ccef193a07
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2024-04-11 12:25:01 +0200

    img program

---
+++ src/PrivatePdfConverter/Commands/ImgToPdf.cs
@@ -0,0 +1,8 @@
+namespace PrivatePdfConverter.Commands;
+
+public static class ImgToPdf
+{
+    public static void ConvertImageToOnePdf(string path, string? output)
+    {
+    }
+}
--- src/PrivatePdfConverter/Program.cs
+++ src/PrivatePdfConverter/Program.cs
@@ -7,6 +7,7 @@ var app = CoconaLiteApp.Create();
 app.AddCommand("dir", DirToPdf.ConvertDirectoryToOnePdf).WithDescription("Converts all images inside directory to single pdf file.");
 app.AddCommand("merge", MergePdf.ConvertDirectoryToOnePdf).WithDescription("Merge pdf files from directory into one pdf file.");
 app.AddCommand("ext", ListValidExt.ListValidExtensions).WithDescription("List all valid image extensions.");
+app.AddCommand("img", ImgToPdf.ConvertImageToOnePdf).WithDescription("Converts single image to single pdf file.");
 
 Logger.Initialize();
 
