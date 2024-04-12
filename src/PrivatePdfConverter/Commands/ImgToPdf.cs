5439603c6bc6751d04c239d639c12dbf68ab487c
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2024-04-12 11:57:30 +0200

    img real code

--- src/PrivatePdfConverter/Commands/ImgToPdf.cs
+++ src/PrivatePdfConverter/Commands/ImgToPdf.cs
@@ -1,8 +1,35 @@
+using ImageMagick;
+using PrivatePdfConverter.Services;
+using Serilog;
+
 namespace PrivatePdfConverter.Commands;
 
 public static class ImgToPdf
 {
     public static void ConvertImageToOnePdf(string path, string? output)
     {
+        if (!Path.GetExtension(path).IsImage())
+        {
+            Log.Logger.Fatal("File type not supported. Please provide a valid image file");
+            // todo log supported file types
+            return;
+        }
+
+        Log.Logger.Information("Read 1 file with name: {FileName}, Full path: '{Path}'", Path.GetFileName(path), path);
+
+        using var images = new MagickImageCollection();
+        images.Add(new MagickImage(path));
+
+        var outputFileName = string.IsNullOrEmpty(output) ? "output.pdf" : output;
+
+        // check if filename already has .pdf extension and add .pdf at the end if
+        if (!outputFileName.EndsWith(".pdf"))
+        {
+            outputFileName += ".pdf";
+        }
+
+        images.Write($"{Path.GetDirectoryName(path)}/{outputFileName}");
+
+        Log.Logger.Information("PDF '{OutputFileName}' created at '{Path}'", outputFileName, $"{path}/{outputFileName}");
     }
 }
