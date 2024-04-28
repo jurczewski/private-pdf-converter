6d1ecb04ad7309832c23787a0240570ef9d951c3
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2024-04-28 20:17:20 +0200

    add period to extension (format)

--- src/PrivatePdfConverter/Commands/ListValidExt.cs
+++ src/PrivatePdfConverter/Commands/ListValidExt.cs
@@ -6,5 +6,5 @@ namespace PrivatePdfConverter.Commands;
 public static class ListValidExt
 {
     public static void ListValidExtensions()
-        => Log.Logger.Information("Valid image extension: {Extension}", string.Join(", ", FileService.ValidExtensions));
+        => Log.Logger.Information("Valid image extension: {Extension}", string.Join(", .", FileService.ValidExtensions));
 }
