038e2c796cdb7c0b70e55af1b8300637f843ad3f
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2024-05-15 10:59:17 +0200

    tests: to file-scope namespace

--- tests/PrivatePdfConverter.UnitTests/DirToPdfIntegrationUnitTests.cs
+++ tests/PrivatePdfConverter.UnitTests/DirToPdfIntegrationUnitTests.cs
@@ -4,48 +4,47 @@ using ImageMagick;
 using PrivatePdfConverter.Commands;
 using Xunit;
 
-namespace PrivatePdfConverter.UnitTests
+namespace PrivatePdfConverter.UnitTests;
+
+public class DirToPdfIntegrationUnitTests
 {
-    public class DirToPdfIntegrationUnitTests
+    [Fact]
+    public void ConvertDirectoryToOnePdf_ShouldBehaveCorrectly()
     {
-        [Fact]
-        public void ConvertDirectoryToOnePdf_ShouldBehaveCorrectly()
+        // Arrange
+        var fixture = new Fixture();
+        var inputDirectoryName = fixture.Create<string>();
+        var inputDirPath = Path.Combine(Path.GetTempPath(), inputDirectoryName);
+        const string outputFileName = "final.pdf";
+        Directory.CreateDirectory(inputDirPath);
+
+        // Create dummy image files
+        var imagePaths = new[]
         {
-            // Arrange
-            var fixture = new Fixture();
-            var inputDirectoryName = fixture.Create<string>();
-            var inputDirPath = Path.Combine(Path.GetTempPath(), inputDirectoryName);
-            const string outputFileName = "final.pdf";
-            Directory.CreateDirectory(inputDirPath);
+            Path.Combine(inputDirPath, "image1.png"),
+            Path.Combine(inputDirPath, "image2.jpg"),
+            Path.Combine(inputDirPath, "image3.bmp")
+        };
 
-            // Create dummy image files
-            var imagePaths = new[]
-            {
-                Path.Combine(inputDirPath, "image1.png"),
-                Path.Combine(inputDirPath, "image2.jpg"),
-                Path.Combine(inputDirPath, "image3.bmp")
-            };
-
-            foreach (var imagePath in imagePaths)
-            {
-                using var image = new MagickImage(MagickColors.Red, 100, 100);
-                image.Write(imagePath);
-            }
-
-            // Act
-            DirToPdf.ConvertDirectoryToOnePdf(inputDirPath, outputFileName);
-
-            // Assert
-            // Check if there is any pdf file created
-            var outputPdfPath = Directory.GetFiles(inputDirPath, "*.pdf").FirstOrDefault();
-            File.Exists(outputPdfPath).Should().BeTrue();
-
-            // Check if there is a specif pdf with that name
-            var output = Path.Combine(Path.GetTempPath(), $"{inputDirectoryName}/{outputFileName}");
-            File.Exists(output).Should().BeTrue();
-
-            // Clean up
-            Directory.Delete(inputDirPath, true);
+        foreach (var imagePath in imagePaths)
+        {
+            using var image = new MagickImage(MagickColors.Red, 100, 100);
+            image.Write(imagePath);
         }
+
+        // Act
+        DirToPdf.ConvertDirectoryToOnePdf(inputDirPath, outputFileName);
+
+        // Assert
+        // Check if there is any pdf file created
+        var outputPdfPath = Directory.GetFiles(inputDirPath, "*.pdf").FirstOrDefault();
+        File.Exists(outputPdfPath).Should().BeTrue();
+
+        // Check if there is a specif pdf with that name
+        var output = Path.Combine(Path.GetTempPath(), $"{inputDirectoryName}/{outputFileName}");
+        File.Exists(output).Should().BeTrue();
+
+        // Clean up
+        Directory.Delete(inputDirPath, true);
     }
 }
--- tests/PrivatePdfConverter.UnitTests/EncryptPdfTests.cs
+++ tests/PrivatePdfConverter.UnitTests/EncryptPdfTests.cs
@@ -5,61 +5,60 @@ using iText.Kernel.Pdf;
 using PrivatePdfConverter.Commands;
 using Xunit;
 
-namespace PrivatePdfConverter.UnitTests
+namespace PrivatePdfConverter.UnitTests;
+
+public sealed class EncryptPdfTests : IDisposable
 {
-    public sealed class EncryptPdfTests : IDisposable
+    private const string EncryptedPdfName = "encrypted.pdf";
+    private readonly string _samplePdfPath;
+    private readonly string _encryptedPdfPath;
+    private readonly string _password;
+
+    public EncryptPdfTests()
     {
-        private const string EncryptedPdfName = "encrypted.pdf";
-        private readonly string _samplePdfPath;
-        private readonly string _encryptedPdfPath;
-        private readonly string _password;
+        var fixture = new Fixture();
+        _password = fixture.Create<string>();
 
-        public EncryptPdfTests()
+        var samplePdfName = fixture.Create<string>();
+        _samplePdfPath = Path.Combine(Path.GetTempPath(), samplePdfName);
+        _encryptedPdfPath = Path.Combine(Path.GetTempPath(), EncryptedPdfName);
+    }
+
+    public void Dispose()
+    {
+        if (File.Exists(_samplePdfPath))
+            File.Delete(_samplePdfPath);
+
+        if (File.Exists(_encryptedPdfPath))
+            File.Delete(_encryptedPdfPath);
+    }
+
+    [Fact]
+    public void EncryptPdfAndOpenWithPassword()
+    {
+        // Arrange
+        CreateSamplePdf(_samplePdfPath);
+
+        // Act
+        EncryptPdf.EncryptPdfWithPassword(_samplePdfPath, _password, EncryptedPdfName);
+
+        // Assert
+        // Try to open the encrypted PDF with the password
+        var openEncryptedPdf = () =>
         {
-            var fixture = new Fixture();
-            _password = fixture.Create<string>();
+            using var pdfReader = new PdfReader(_encryptedPdfPath, new ReaderProperties().SetPassword(Encoding.UTF8.GetBytes(_password)));
+            using var pdfDocument = new PdfDocument(pdfReader);
+        };
 
-            var samplePdfName = fixture.Create<string>();
-            _samplePdfPath = Path.Combine(Path.GetTempPath(), samplePdfName);
-            _encryptedPdfPath = Path.Combine(Path.GetTempPath(), EncryptedPdfName);
-        }
+        openEncryptedPdf.Should().NotThrow("because the encrypted PDF should open with the correct password");
+        File.Exists(_encryptedPdfPath).Should().BeTrue("because the encrypted PDF should be created");
+    }
 
-        public void Dispose()
-        {
-            if (File.Exists(_samplePdfPath))
-                File.Delete(_samplePdfPath);
-
-            if (File.Exists(_encryptedPdfPath))
-                File.Delete(_encryptedPdfPath);
-        }
-
-        [Fact]
-        public void EncryptPdfAndOpenWithPassword()
-        {
-            // Arrange
-            CreateSamplePdf(_samplePdfPath);
-
-            // Act
-            EncryptPdf.EncryptPdfWithPassword(_samplePdfPath, _password, EncryptedPdfName);
-
-            // Assert
-            // Try to open the encrypted PDF with the password
-            var openEncryptedPdf = () =>
-            {
-                using var pdfReader = new PdfReader(_encryptedPdfPath, new ReaderProperties().SetPassword(Encoding.UTF8.GetBytes(_password)));
-                using var pdfDocument = new PdfDocument(pdfReader);
-            };
-
-            openEncryptedPdf.Should().NotThrow("because the encrypted PDF should open with the correct password");
-            File.Exists(_encryptedPdfPath).Should().BeTrue("because the encrypted PDF should be created");
-        }
-
-        private static void CreateSamplePdf(string filePath)
-        {
-            using var pdfWriter = new PdfWriter(filePath);
-            using var pdfDocument = new PdfDocument(pdfWriter);
-            var document = new iText.Layout.Document(pdfDocument);
-            document.Add(new iText.Layout.Element.Paragraph("This is a sample PDF."));
-        }
+    private static void CreateSamplePdf(string filePath)
+    {
+        using var pdfWriter = new PdfWriter(filePath);
+        using var pdfDocument = new PdfDocument(pdfWriter);
+        var document = new iText.Layout.Document(pdfDocument);
+        document.Add(new iText.Layout.Element.Paragraph("This is a sample PDF."));
     }
 }
