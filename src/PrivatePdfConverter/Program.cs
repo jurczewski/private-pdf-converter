b9d06d20e13a2f211d12fd440786c2be0c6ee420
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2025-10-08 12:02:27 +0200

    chore: extract register commands to function

--- src/PrivatePdfConverter/Program.cs
+++ src/PrivatePdfConverter/Program.cs
@@ -9,12 +9,22 @@ Logger.Initialize();
 Logger.LogStart(versionString);
 
 var app = CoconaLiteApp.Create();
-
-app.AddCommand("dir", DirToPdf.ConvertDirectoryToOnePdf).WithDescription("Convert all images inside directory to single pdf file.");
-app.AddCommand("merge", MergePdf.ConvertDirectoryToOnePdf).WithDescription("Merge pdf files from directory into one pdf file.");
-app.AddCommand("ext", ListValidExt.ListValidExtensions).WithDescription("List all valid image extensions.");
-app.AddCommand("img", ImgToPdf.ConvertImageToOnePdf).WithDescription("Convert single image to single pdf file.");
-app.AddCommand("encrypt", EncryptPdf.EncryptPdfWithPassword).WithDescription("Encrypt pdf file with a password.");
-app.AddCommand("logo", AddLogoToPdf.Run).WithDescription("Add an image/logo/watermark to a every page of pdf file.");
+RegisterCommands(app);
 
 await app.RunAsync();
+
+static void RegisterCommands(CoconaLiteApp app)
+{
+    app.AddCommand("dir", DirToPdf.ConvertDirectoryToOnePdf)
+        .WithDescription("Convert all images inside directory to single pdf file.");
+    app.AddCommand("merge", MergePdf.ConvertDirectoryToOnePdf)
+        .WithDescription("Merge pdf files from directory into one pdf file.");
+    app.AddCommand("ext", ListValidExt.ListValidExtensions)
+        .WithDescription("List all valid image extensions.");
+    app.AddCommand("img", ImgToPdf.ConvertImageToOnePdf)
+        .WithDescription("Convert single image to single pdf file.");
+    app.AddCommand("encrypt", EncryptPdf.EncryptPdfWithPassword)
+        .WithDescription("Encrypt pdf file with a password.");
+    app.AddCommand("logo", AddLogoToPdf.Run)
+        .WithDescription("Add an image/logo/watermark to a every page of pdf file.");
+}
